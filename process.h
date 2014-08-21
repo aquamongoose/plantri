e70383f42d71313862bff349bdfe732cfec8fce2
Joshua Brakensiek <jbrakensiek@users.noreply.github.com>
2014-08-21 15:22:44 -0400

    Fixed bug with zero

--- process.h
+++ process.h
@@ -5,9 +5,9 @@
 // prints the number of unique vertex assignments which
 // correspond to face colorings. If zero != 0, then the
 // possible set is {-1, 0, 1}; else it is {-1, 1}
-void process (plantri* tri, int zero);
+void process (plantri* tri, bool zero);
 
 // prints the vertex assignments which correspond to face
 // colorings. Similar to process.
-void process_all (plantri* tri, int zero);
+void process_all (plantri* tri, bool zero);
 #endif
